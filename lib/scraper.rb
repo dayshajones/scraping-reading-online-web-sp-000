a425beb0b85d0df9817f41baddda9539169168b4
Daysha Jones <daysha.vu@icloud.com>
2021-03-22 17:23:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-Nokogiri::HTML(html)
-doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline")
+
+doc.css(".tout__label.heading.heading--level-4")[0]
+
